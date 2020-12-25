587bb9cf937c04cc53e496fbfc0c9e41481529f9
furbasolta <foorvaa.grg@gmail.com>
2020-12-25 12:54:08 +0545

    init

---
+++ js/gsap.js
@@ -0,0 +1,61 @@
+
+gsap.from('.logo', 1, {
+
+    y: '-100%',
+    ease: 'power1'
+  });
+  gsap.from('.hamburger-menu', 1, {
+    delay: 1,
+    x: '100vw',
+    ease: 'power1',
+  })
+  gsap.from('.nav1', .5, {
+    delay: 2,
+  
+    ease: 'slow',
+    stagger: 1
+  })
+  gsap.from('#scene', 2, {
+  
+    delay: 1,
+    ease: 'slow',
+    stagger: 1
+  })
+  
+  gsap.from('#menu1', 1, {
+  
+  
+    x: '-200px',
+  
+  
+  })
+  gsap.from('#menu2', 1, {
+  
+    x: '200px',
+  
+  
+  })
+  gsap.from('#menu3', 1, {
+  
+  
+    y: '200px',
+  
+  
+  })
+  gsap.from('#dash1', 1, {
+    delay: 1,
+    x: '-150px',
+  })
+  gsap.from('#dash2', 1, {
+    delay: 1,
+    x: '150px',
+  })
+  gsap.from('#dash3', 1, {
+    delay: 1,
+    y: '150px',
+  })
+  
+  
+  
+  
+  
